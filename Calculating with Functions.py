173fcfe2a2f52b3adb4f9a89c15dd3630cbd70d7
Mohamed Aziz Ammar <108346635+aziz4ammar@users.noreply.github.com>
2023-08-27 21:24:03 +0100

    Final Commit

---
+++ Calculating with Functions.py
@@ -0,0 +1,14 @@
+def zero(op=None): return op(0) if op else 0
+def one(op=None): return op(1) if op else 1
+def two(op=None): return op(2) if op else 2
+def three(op=None): return op(3) if op else 3
+def four(op=None): return op(4) if op else 4
+def five(op=None): return op(5) if op else 5
+def six(op=None): return op(6) if op else 6
+def seven(op=None): return op(7) if op else 7
+def eight(op=None): return op(8) if op else 8
+def nine(op=None): return op(9) if op else 9
+def plus(num): return lambda x: x + num
+def minus(num): return lambda x: x - num
+def times(num): return lambda x: x * num
+def divided_by(num): return lambda x: x // num
